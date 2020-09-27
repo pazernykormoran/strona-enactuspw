769ea3afec107b904a12a7d46366f639dee1a722
Andrzej <andrzolide@gmail.com>
2020-09-27 12:52:31 +0200

    initial commit

---
+++ formularz.php
@@ -0,0 +1,5 @@
+<?php
+echo "formularz";
+header("Location: https://forms.office.com/Pages/ResponsePage.aspx?id=nCJQO3jNiEWbz5e3Yp4vD-JxlHD7k9JLrfwFRFo_5nhUMThVVVBXWUlVOVczNVRDSjdBN0hRQ0tWSC4u");
+?>
+
